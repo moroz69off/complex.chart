f0e21f83e0456d696d6c2320e2123bd42e24ccdd
Морозов Юрий <moroz69off@gmail.com>
2021-05-01 11:47:16 +0300

    add json
    Signed-off-by: Морозов Юрий <moroz69off@gmail.com>

--- app/js/app.js
+++ app/js/app.js
@@ -23,7 +23,27 @@ check_items.forEach(
 		value.addEventListener('onclick', mf(value));
 	}
 );
+
 function mf (value) {
-	
+	console.log(value);
 }
+
+//==============================================================
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("/charts/response.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
 
